2206489cab20797d13fa012b652a2c8d34dc1b32
strivingwang <pure1114@126.com>
2016-08-31 15:18:33 +0800

    remote edit test
    remote edit test

--- src/com/wang/git/GitDemo.java
+++ src/com/wang/git/GitDemo.java
@@ -2,6 +2,6 @@ package com.wang.git;
 
 public class GitDemo {
 	public static void main(String[] args){
-		System.out.print("git demo test !");
+		System.out.print("git demo test !");//远端合并到本地测试
 	}
 }
